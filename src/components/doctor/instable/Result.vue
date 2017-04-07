e78c56b48a03e235c7f1a0274455c644e99ccbd1
chawki benchehida <chawki27000@hotmail.com>
2017-04-07 02:43:10 +0100

    add pulmonaire result in result view

--- src/components/doctor/instable/Result.vue
+++ src/components/doctor/instable/Result.vue
@@ -18,6 +18,18 @@
 
   <section class="pleuro">
     <h2>Resultat Pleuro-pulmonaire</h2>
+    <div class="info">
+      <h3>FC : </h3>
+      <p>{{pleuro.fr}}</p>
+    </div>
+    <div class="info">
+      <h3>Amplitude : </h3>
+      <p>{{pleuro.ampl}}</p>
+    </div>
+    <div class="info">
+      <h3>Resp : </h3>
+      <p>{{pleuro.resp}}</p>
+    </div>
   </section>
 
   <section class="neuro">
@@ -28,30 +40,49 @@
 
 <script>
 export default {
-
     data() {
         return {
             hemo: {
                 fc: '',
                 ta: '',
                 temp: '',
+            },
+            pleuro:  {
+                fr: '',
+                resp: '',
+                ampl: ''
             }
         }
     },
 
     mounted () {
-        var resource = this.$resource('expert/on');
+        // Hemodynamic expert
+        var resource = this.$resource('expert/on/1')
         resource.save({
             child_id: localStorage.getItem('child_id'),
             hemo_id: localStorage.getItem('hemo_id'),
-            pleuro_id: localStorage.getItem('pleuro_id')
         }).then(response => {
             console.log(response.body);
             this.hemo.fc = response.body.fc;
             this.hemo.ta = response.body.ta;
             this.hemo.temp = response.body.temp;
         }, response => {
-            console.error(response.body);
+            console.error(response)
+        })
+
+        // Pleuro-pulmonaire expert
+        var resource2 = this.$resource('expert/on/2')
+        resource2.save({
+            child_id: localStorage.getItem('child_id'),
+            pleuro_id: localStorage.getItem('pleuro_id'),
+        }).then(response => {
+            console.log(response)
+            var object = JSON.parse(response.body.body)
+            this.pleuro.fr = object.fr
+            this.pleuro.resp = object.resp
+            this.pleuro.ampl = response.body.ampl
+        }, response => {
+            console.error(response)
         })
     }
 }
